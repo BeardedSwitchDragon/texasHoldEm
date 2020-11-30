c20050cbfc09c812d53e419ff142c2d99efcac38
Geddy Mukama <geddymaria.mukama@gmail.com>
2020-11-30 17:25:37 +1100

    added a way to determine card values & a getter for the card.

--- src/texasHoldEm/Card.java
+++ src/texasHoldEm/Card.java
@@ -1,7 +1,25 @@
 package texasHoldEm;
 
+import java.util.*;
 public class Card {
+	String rank;
+	String suit;
+	int rankValue;
+	
+	String[] ranks = {"Jkr", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"};
+	String[]  suits = {"♦", "♣", "♥", "♠"};
 	Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+		
+		
+		this.rankValue = Arrays.asList(ranks).indexOf(this.rank);
+		
 		
 	}
+	
+	public String getCard() {
+		return this.rank + this.suit;
+	}
+	
 }
--- src/texasHoldEm/Poker.java
+++ src/texasHoldEm/Poker.java
@@ -3,6 +3,7 @@ package texasHoldEm;
 public class Poker {
 	
 	public static void main(String args[]) {
-		
+		Card card = new Card("5", "♠");
+		System.out.println(card.getCard());
 	}
 }
