cce195b981605e4b9ebc484d016a3d5ddce1b291
Geddy Mukama <geddymaria.mukama@gmail.com>
2020-12-01 16:29:32 +1100

    reverting to 1056888

--- src/texasHoldEm/Hand.java
+++ src/texasHoldEm/Hand.java
@@ -1,14 +1,16 @@
 package texasHoldEm;
 import java.util.*;
-public class Deck {
+public class Hand {
 	Card[] cards = new Card[5];
 	int deckValue;
-	String[] cardsValue = new String[5];
+	String[] cardsRank = new String[5];
+	String[] cardsSuit = new String[5];
 	
-	Deck() {
+	Hand() {
 		for (int i = 0; i < 5; i++) {
 			cards[i] = new Card(Card.ranks[i], "♦");
-			cardsValue[i] = cards[i].rank;
+			cardsRank[i] = cards[i].rank;
+			
 		}
 		
 		for (Card card : cards) {
@@ -22,7 +24,7 @@ public class Deck {
 		for (int i = 0; i < Card.ranks.length; i++) {
 			
 			String[] sample = Arrays.copyOfRange(Card.ranks, i, i+5);
-			if (Arrays.equals(sample, cardsValue)) {
+			if (Arrays.equals(sample, cardsRank)) {
 				
 				for (int j = 1; j < cards.length; j++) {
 					if (cards[j].suit != cards[j - 1].suit) {
@@ -32,7 +34,25 @@ public class Deck {
 					if (j == (cards.length - 1)) return 9;
 				}
 				
-				return 5;
+				return 4;
+			}
+		}
+		return 0;
+	}
+	
+	
+	public int checkFlush() {
+		for (int i = 1; i < 5; i++) {
+			if (cards[i-1].suit != cards[i].suit) {
+				if (i == 4) {
+					if (deckValue >= 60) {
+						return 10;
+					} else {
+						return 5;
+					}
+				}
+			} else {
+				break;
 			}
 		}
 		return 0;
--- src/texasHoldEm/Poker.java
+++ src/texasHoldEm/Poker.java
@@ -3,7 +3,7 @@ package texasHoldEm;
 public class Poker {
 	
 	public static void main(String args[]) {
-		Hand testDeck = new Hand();
+		Deck testDeck = new Deck();
 		System.out.println(testDeck.deckValue);
 	}
 }
